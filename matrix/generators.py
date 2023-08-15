f52ea8d7f45199a08ed6a76b77f159d7c08dead6
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-08-15 17:20:07 +0200

    Updated deps

--- matrix/generators.py
+++ matrix/generators.py
@@ -1,10 +1,10 @@
+from random import randint as random_int
 
-def random_matrix(rows=4, cols=4, min_value=1, max_value=9)->list:
+def random_matrix_int(rows=4, cols=4, min_value=1, max_value=9)->list:
     """
     Returns a matrix of random values.
     """
-    import random
-    return [[random.randint(min_value, max_value) for _ in range(cols)] for _ in range(rows)]
+    return [[random_int(min_value, max_value) for _ in range(cols)] for _ in range(rows)]
 
 class Random:
   def __init__(self, rows=4, columns=4, max_value=9, min_value=1) -> None:
@@ -12,10 +12,25 @@ class Random:
     self.columns = columns
     self.max_value = max_value
     self.min_value = min_value
-    self.matrix = random_matrix(self.rows, self.columns, self.min_value, self.max_value)
+    self.matrix = random_matrix_int(self.rows, self.columns, self.min_value, self.max_value)
+
+  def __str__(self) -> str:
+    return f"Random Matrix of {self.rows} rows and {self.columns} columns"
+
+  def __repr__(self) -> str:
+    return f"Random({self.rows}, {self.columns}, {self.max_value}, {self.min_value})"
+
   def pretty_print(self):
     """
     Prints the matrix in a pretty format.
     """
     for row in self.matrix:
       print(*row)
+  
+  def get_item(self, x, y):
+    """
+    Returns the value at the given index
+    """
+    if x < 0 or y < 0 or x >= self.rows or y >= self.columns:
+      raise IndexError("Index out of range")
+    return self.matrix[x][y]
--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,5 +1,6 @@
 import generators
 
 if __name__ == "__main__":
-  rand_matrix = generators.Random()
+  rand_matrix = generators.Random(12, 12)
+  print(rand_matrix.get_item(11, 11))
   rand_matrix.pretty_print()
